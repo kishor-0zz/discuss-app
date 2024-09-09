543f1b2a0a59f4a866e2241117cb8fd9eca9648a
kishor saha <kishorsaha650@gmail.com>
2024-09-10 00:49:54 +0600

    db set

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body>
         <Provider>
-        {children}
+          {children}
         </Provider>
       </body>
     </html>
